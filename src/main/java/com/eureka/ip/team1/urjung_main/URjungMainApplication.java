4f7e18c9ab52dc10c428f5658b9417ce0c0374df
suhyun <suhyun9764@naver.com>
2025-06-04 17:23:52 +0900

    init

---
+++ src/main/java/com/eureka/ip/team1/urjung_main/URjungMainApplication.java
@@ -0,0 +1,13 @@
+package com.eureka.ip.team1.urjung_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class URjungMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(URjungMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eureka/ip/team1/urjung_main/URjungMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.ip.team1.urjung_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class URjungMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
