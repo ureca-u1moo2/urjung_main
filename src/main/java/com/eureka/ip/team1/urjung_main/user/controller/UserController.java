25774acdcd4833878adbd4e3b777e06c93455710
rxgx424 <gkfudrd1234@gmail.com>
2025-06-11 13:32:01 +0900

    아이디/비밀번호 찾기 기능 구현

---
+++ src/main/java/com/eureka/ip/team1/urjung_main/user/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.eureka.ip.team1.urjung_main.user.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@Slf4j
+public class UserController {
+
+
+}
