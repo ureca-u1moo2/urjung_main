48af3cf89198a6a2efac49a71beac01b4fe6975d
suhyun <suhyun9764@naver.com>
2025-06-16 17:27:24 +0900

    refactor : UserDetails 추가

--- src/main/java/com/eureka/ip/team1/urjung_main/chatbot/controller/ChatBotController.java
+++ src/main/java/com/eureka/ip/team1/urjung_main/chatbot/controller/ChatBotController.java
@@ -1,10 +1,12 @@
 package com.eureka.ip.team1.urjung_main.chatbot.controller;
 
+import com.eureka.ip.team1.urjung_main.auth.config.CustomUserDetails;
 import com.eureka.ip.team1.urjung_main.chatbot.dto.ChatRequestDto;
 import com.eureka.ip.team1.urjung_main.chatbot.dto.ChatResponseDto;
 import com.eureka.ip.team1.urjung_main.chatbot.facade.ChatInteractionFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +20,7 @@ public class ChatBotController {
     private final ChatInteractionFacade chatInteractionFacade;
 
     @PostMapping(produces = MediaType.APPLICATION_NDJSON_VALUE)
-    public Flux<ChatResponseDto> chat(@RequestBody ChatRequestDto requestDto) {
-        return chatInteractionFacade.chat("1", requestDto);
+    public Flux<ChatResponseDto> chat(@AuthenticationPrincipal CustomUserDetails userDetails, @RequestBody ChatRequestDto requestDto) {
+        return chatInteractionFacade.chat(userDetails.getUserId(), requestDto);
     }
 }
