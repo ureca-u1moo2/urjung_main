08cc972cce894484546727ec4703d0b40d141299
Jeong Seong Bin <tjdqls3124@gmail.com>
2025-06-16 11:00:57 +0900

    Create ForbiddenWordInternalControllerTest.java
    feat: 금칙어 갱신 컨트롤러 테스트코드 (#70 )

---
+++ src/test/java/com/eureka/ip/team1/urjung_main/chatbot/controller/ForbiddenWordInternalControllerTest.java
@@ -0,0 +1,43 @@
+package com.eureka.ip.team1.urjung_main.chatbot.controller;
+
+import com.eureka.ip.team1.urjung_main.chatbot.controller.ForbiddenWordController;
+import com.eureka.ip.team1.urjung_main.chatbot.service.ForbiddenWordService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@WebMvcTest(ForbiddenWordInternalController.class)
+@AutoConfigureMockMvc(addFilters = false)
+class ForbiddenWordInternalControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ForbiddenWordService forbiddenWordService;
+
+    @Test
+    @DisplayName("금칙어 갱신 API 호출 시 200 OK 응답과 메시지를 반환해야 한다")
+    void reloadForbiddenWordsTest() throws Exception {
+        doNothing().when(forbiddenWordService).reloadForbiddenWords();
+
+        mockMvc.perform(post("/internal/forbidden/reload")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("금칙어 갱신 완료"));
+
+        Mockito.verify(forbiddenWordService).reloadForbiddenWords();
+    }
+}
